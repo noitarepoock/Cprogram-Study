46bd6b193156391567ce1554774e4d168694c252
noitarepoock <150671196+noitarepoock@users.noreply.github.com>
2025-08-10 12:49:29 +0000

    puts, fputs

--- gituse.c
+++ gituse.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, Codespaces!\n");
+    char str[80] = "apple juice";
+    char *ps = "banana";
+
+    puts(str);
+    fputs(ps, stdout);
+    printf("\n");
+    puts("milk");
+
     return 0;
 }
