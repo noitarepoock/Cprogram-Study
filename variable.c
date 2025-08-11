6a85bc44c3302a9a35dab5632ac95e28a363642b
noitarepoock <150671196+noitarepoock@users.noreply.github.com>
2025-08-11 13:59:12 +0000

    variable

--- variable.c
+++ variable.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 #include <string.h>
 
+void assign(void);
+
 int main() {
+    // auto int a = 20;        //in main function variable a
+
+    // assign();
+    // printf("main function a : %d\n", a);
+
+
     
+
+    return 0;
+}
+
+void assign(void){
+    int a;      //in assign function varialbe a
+
+    a = 10;
+    printf("assign function a : %d\n", a);
 }
