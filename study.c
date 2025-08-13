002dc7e84a5e09bafb66f8603fd81a26622baff7
noitarepoock <150671196+noitarepoock@users.noreply.github.com>
2025-08-13 14:20:55 +0000

    study

--- study.c
+++ study.c
@@ -75,21 +75,36 @@ int main(){
     printf("another string : %s", my_strcpy(str, "kiwi"));
     */          //strcpy와 같은 기능을 하는 함수 직접 만들기
 
-    char str1[15], str2[15];
-    int i = 5;
+    // char str1[15], str2[15];             //12-2 Q3
+    // int i = 5;
 
-    printf("string : ");
-    scanf("%s", str2);
+    // printf("string : ");
+    // scanf("%s", str2);
 
-    strcpy(str1, str2);
-    if(strlen(str2) > 5){
-        while(str2[i] != '\0'){
-            str2[i] = '*';
-            i++;
-        }
-    }
+    // strcpy(str1, str2);
+    // if(strlen(str2) > 5){
+    //     while(str2[i] != '\0'){
+    //         str2[i] = '*';
+    //         i++;
+    //     }
+    // }
 
-    printf("oriString : %s, hiddenString : %s", str1, str2);
+    // printf("oriString : %s, hiddenString : %s", str1, str2);
+
+
+
+    // char str1[15], str2[15], str3[15];                   //12 - 2 Q4
+    
+
+    // printf("three fruits : ");
+    // scanf("%s %s %s", str1, str2, str3);
+
+    // if(strcmp(str1,str2) < 0 && strcmp(str1, str3) < 0 && strcmp(str2, str3) < 0) printf("%s, %s, %s", str1, str2, str3);
+    // else if(strcmp(str1,str2) < 0 && strcmp(str1, str3) < 0 && strcmp(str2, str3) > 0) printf("%s, %s, %s", str1, str3, str2);
+    // else if(strcmp(str1,str2) > 0 && strcmp(str1, str3) < 0 && strcmp(str2, str3) < 0) printf("%s, %s, %s", str2, str1, str3);
+    // else if(strcmp(str1,str2) > 0 && strcmp(str1, str3) > 0 && strcmp(str2, str3) < 0) printf("%s, %s, %s", str2, str3, str1);
+    // else if(strcmp(str1,str2) < 0 && strcmp(str1, str3) > 0 && strcmp(str2, str3) > 0) printf("%s, %s, %s", str3, str1, str2);
+    // else if(strcmp(str1,str2) > 0 && strcmp(str1, str3) > 0 && strcmp(str2, str3) > 0) printf("%s, %s, %s", str3, str2, str1);
 
     return 0;
 }
