0080a89ab8cba5f70fe95ea9ec93b83a83c432d5
noitarepoock <150671196+noitarepoock@users.noreply.github.com>
2025-08-13 14:44:03 +0000

    variable

--- variable.c
+++ variable.c
@@ -6,8 +6,16 @@ void assign10(void);
 void assign20(void);
 void auto_func(void);
 void static_func(void);
+int add_ten(int a);
+void add_r_ten(int *pa);            //addByAddress
+int *sum(int a, int b);
+void input_data(int *pa, int *pb);
+void swap_data(void);
+void print_data(int a, int b);
 
 // int a;          //global variable a
+
+// int a, b;        //13-2 Q4(도전 실전 예제)
 
 int main() {
     // auto int a = 20;        //in main function variable a
@@ -53,14 +61,37 @@ int main() {
     // }
 
 
-    register int i;         //store variable in register in CPU
-    int sum = 0;
+    // register int i;         //store variable in register in CPU
+    // int sum = 0;
 
-    for(i = 1; i <= 10000; i++){
-        sum += i;
-    }
+    // for(i = 1; i <= 10000; i++){
+    //     sum += i;
+    // }
 
-    printf("%d\n", sum);
+    // printf("%d\n", sum);
+
+
+    // int a = 10;
+
+    // // add_ten(a);             //copy a to add_ten function
+    // printf("a : %d\n", add_ten(a));
+
+
+    // int a = 10;
+
+    // add_r_ten(&a);
+    // printf("a : %d\n", a);
+
+    // int *resp;
+
+    // resp = sum(10, 20);             //resp = &res in *sum function
+    // printf(" adding two num : %d", *resp);
+
+
+
+    // input_data(&a, &b);          //13-2 Q4(도전 실전 예제)
+    // swap_data();
+    // print_data(a, b);
 
     return 0;
 }
@@ -94,4 +125,37 @@ void static_func(void){
 
     a++;
     printf("%d\n", a);
+}
+
+int add_ten(int a){
+    a = a + 10;
+
+    return a;
+}
+
+void add_r_ten(int *pa){
+    *pa += 10;
+}
+
+int *sum(int a, int b){
+    static int res;
+    
+    res = a + b;
+
+    return &res;
+}
+
+void input_data(int *pa, int *pb){
+
+    printf("input two num : ");
+    scanf("%d %d", pa, pb);
+}
+void swap_data(void){
+    int tmp;
+    tmp = a;
+    a = b;
+    b = tmp;
+}
+void print_data(int a, int b){
+    printf("output two num : %d, %d\n",a, b);
 }
