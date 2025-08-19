723b699140988e03b2d532bd474379da1e8d2888
noitarepoock <150671196+noitarepoock@users.noreply.github.com>
2025-08-19 14:40:58 +0000

    arr, ptr

--- arr.c
+++ arr.c
@@ -3,6 +3,7 @@
 
 void swqp_ptr(char **ppa, char **ppb);
 void print_str(char **pps, int cnt);
+void print_ary(int (*pa)[4]);
 
 int main(void){
     // int score[3][4];
@@ -165,12 +166,43 @@ int main(void){
 
     
 
+    // int ary[3][4] = { {1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12} };
+    // int (*pa)[4];
+
+    // pa = ary;
+    // for(int i = 0; i < 3; i++){
+    //     for(int j = 0; j < 4; j++){
+    //         printf("%5d", pa[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+
+
+
+    int ary[3][4] = { {1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12} };
+
+    // print_ary(ary);
+    printf("%d\n", sizeof(&ary[0]));
+    printf("%d\n", sizeof(ary[0]));    
+    printf("%d\n", sizeof(ary));
+    printf("%d", sizeof(&ary[0][0]));
+
     return 0;
 }
 
 void print_str(char **pps, int cnt){
     for(int i = 0; i < cnt; i++){
         printf("%s\n", pps[i]);
+    }
+}
+
+void print_ary(int (*pa)[4])
+{
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 4; j++){
+            printf("%5d", pa[i][j]);
+        }
+        printf("\n");
     }
 }
 
