4c7c42de6b751cb678607c84304ee4ce6feabbe7
noitarepoock <150671196+noitarepoock@users.noreply.github.com>
2025-09-25 12:48:26 +0000

    last..

---
+++ last.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include "student.h"
+#define PI 3.14159
+#define LIMIT 100.0
+#define MSG "passed!"
+#define ERR_PRN printf("허용 범위를 벗어났습니다!\n")
+#define SUM(a, b) ((a) + (b))
+#define MUL(a, b) ((a) * (b))
+#define VER 7
+#define BIT16
+
+
+int main(void){
+    // Student a = {315, "홍길동"};
+
+    // printf("sid : %d, name : %s", a.num, a.name);
+
+
+
+    // double radius, area;
+
+    // printf("반지름 입력 : ");
+    // scanf("%lf", &radius);
+    // area = PI * radius * radius;
+    // if(area > LIMIT) ERR_PRN;
+    // else printf("area : %.2lf (%s)\n", area, MSG);
+
+
+
+    // int a = 10, b = 20;
+    // int x = 30, y = 40;
+    // int res;
+
+    // printf("a + b = %d\n", SUM(a, b));
+    // printf("x + y = %d\n", SUM(x, y));
+    // res = 30 / MUL(2, 5);
+    // printf("res : %d\n", res);
+
+
+
+//     int max;
+
+// #if VER >=6
+//     printf("version %d\n", VER);
+// #endif
+
+// #ifdef BIT16
+//     max = 32767;
+// #else
+//     max = 2147483647;
+// #endif
+
+//     printf("int 형 변수의 최대값 : %d\n", max);
+
+
+
+    
+    
+
+    return 0;
+}
