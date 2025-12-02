d26df57e8b5da9a45e5633707a6be4fdd4035ddd
noitarepoock <150671196+noitarepoock@users.noreply.github.com>
2025-12-02 13:23:52 +0000

    singly linkedList

--- list.c
+++ list.c
@@ -81,6 +81,38 @@ void SLL_InsertNode(Node *Current, Node *NewNode){      //changing only element 
     Current->NextNode = NewNode;
 }
 
+//VITAMIN QUIZ
+void SLL_InsertBefore(Node **Head, Node *Current, Node *NewNode){
+    if((*Head) == NULL){
+        (*Head) = NewNode;
+    }
+    if((*Head) == Current){
+        NewNode->NextNode = (*Head);
+        (*Head) = NewNode;
+    }
+    else{
+        Node *Before = (*Head);
+        while(Before->NextNode != Current){
+            Before = Before->NextNode;
+        }
+        Before->NextNode = NewNode;
+        NewNode->NextNode = Current;
+    }
+}
+
+void SLL_DsetroyAllNodes(Node **List){
+    Node *Current = *List;
+    Node *Next = NULL;
+
+    while(Current != NULL){
+        Next = Current->NextNode;   // 다음 노드 저장
+        free(Current);              // 현재 노드 삭제
+        Current = Next;             // 다음 노드로 이동
+    }
+
+    *List = NULL;  // 리스트 완전 초기화
+}
+
 int SLL_GetNodeCount(Node *Head){
     int Count = 0;
     Node *Current = Head;
@@ -93,22 +125,76 @@ int SLL_GetNodeCount(Node *Head){
     return Count;
 }
 
+void SLL_InsertNewHead(Node **Head, Node *NewHead){
+    if((*Head) == NULL){
+        (*Head) = NewHead;
+    }
+    else{
+        NewHead->NextNode = (*Head);
+        (*Head) = NewHead;
+    }
+}
+
 int main(void){
+    int i = 0;
+    int Count = 0;
     Node *List = NULL;
+    Node *Current = NULL;
     Node *NewNode = NULL;
-    Node *MyNode = NULL;
-    NewNode = SLL_CreateNode(117);
-    SLL_AppendNode(&List, NewNode);
-    NewNode = SLL_CreateNode(119);
-    SLL_AppendNode(&List, NewNode);
-    SLL_AppendNode(&List, SLL_CreateNode(212));
 
-    MyNode = SLL_GetNodeAt(List, 1);
-    printf("%d\n", MyNode->Data);
+    //add 5nodes
+    for(i = 0; i < 5; i++){
+        NewNode = SLL_CreateNode(i);
+        SLL_AppendNode(&List, NewNode);
+    }
 
-    SLL_RemoveNode(&List, MyNode);
-    SLL_DeleteNode(MyNode);
+    NewNode = SLL_CreateNode(-1);
+    SLL_InsertNewHead(&List, NewNode);
 
+    NewNode = SLL_CreateNode(-2);
+    SLL_InsertNewHead(&List, NewNode);
+
+    //print LIST
+    Count = SLL_GetNodeCount(List);
+    for(i = 0; i < Count; i++){
+        Current = SLL_GetNodeAt(List, i);
+        printf("List[%d] : %d\n", i, Current->Data);
+    }
+
+    printf("\nInserting 3000 After [2]...\n\n");
+
+    Current = SLL_GetNodeAt(List, 2);
+    NewNode = SLL_CreateNode(3000);
+
+    SLL_InsertNode(Current, NewNode);
+
+    //print LIST
+    Count = SLL_GetNodeCount(List);
+    for(i = 0; i < Count; i++){
+        Current = SLL_GetNodeAt(List, i);
+        printf("List[%d] : %d\n", i, Current->Data);
+    }
+
+    printf("\nDestroying List...\n");
+
+    // for(i = 0; i < Count; i++){
+    //     Current = SLL_GetNodeAt(List, 0);
+
+    //     if(Current != NULL){
+    //         SLL_RemoveNode(&List, Current);
+    //         SLL_DeleteNode(Current);
+    //     }
+    // }
+
+    SLL_DsetroyAllNodes(&List);
+
+    Count = SLL_GetNodeCount(List);
+    for(i = 0; i < Count; i++){
+        Current = SLL_GetNodeAt(List, i);
+        printf("List[%d] : %d\n", i, Current->Data);
+    }
+
+    if(Count == 0) printf("Emp");
 
     return 0;
 }
